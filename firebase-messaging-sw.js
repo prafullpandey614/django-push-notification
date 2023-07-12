5edb9b32e12a9dd87c803c25be471f7d50a5bb02
prafullpandey614 <prafullpandey614@gmail.com>
2023-07-12 14:45:19 +0530

    sw worker updated

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
     apiKey: "AIzaSyDTAb425y_oiYZCGT9Fi4ZnqsYaqG6qXB4",
